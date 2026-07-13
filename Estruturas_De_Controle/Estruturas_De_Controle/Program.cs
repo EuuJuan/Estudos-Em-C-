add5029d6bfcfe69a6f24d1ac01b8e64f4eacefb
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-13 15:59:01 -0300

    Add files via upload

---
+++ Estruturas_De_Controle/Estruturas_De_Controle/Program.cs
@@ -0,0 +1,47 @@
+﻿//Nesse codigo estudei como funciona as estruturas de controle em C# 
+
+/*int hora;
+ Console.Write("Digite uma hora do dia: ");
+ hora = int.Parse(Console.ReadLine()!);
+
+ if (hora < 12) {
+    Console.WriteLine("Bom dia!");
+  }
+ else if (hora < 18){
+    Console.WriteLine("Boa tarde!");
+ }
+else {
+    Console.WriteLine("Boa noite!");
+}*/
+
+//Soma dos numeros com While
+/*int x, soma;
+
+soma = 0;
+Console.Write("Digite o primeiro numero: ");
+x = int.Parse(Console.ReadLine()!);
+
+while (x != 0) {
+    soma = soma + x;
+    Console.Write("Digite outro numero: ");
+    x = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine("Soma dos numeros é = " + soma);
+Console.WriteLine("\nPrograma Finalizado.\n");*/
+
+//Soma dos numeros com For
+/*int N, x, soma,i;
+
+soma = 0;
+Console.Write("Digite o primeiro numero: ");
+N = int.Parse(Console.ReadLine()!);
+
+for (i = 1; i <= N; i++) {
+    Console.Write("Digite outro numero: ");
+    x = int.Parse(Console.ReadLine()!);
+    soma = soma + x;
+}
+
+Console.WriteLine("Soma dos numeros é = " + soma);
+Console.WriteLine("\nPrograma Finalizado.\n");*/
