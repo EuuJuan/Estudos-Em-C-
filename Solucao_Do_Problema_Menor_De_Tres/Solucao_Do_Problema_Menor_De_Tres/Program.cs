4b84986b14c6e863fb498801dd369bfa591957f4
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:47:58 -0300

    Add files via upload

---
+++ Solucao_Do_Problema_Menor_De_Tres/Solucao_Do_Problema_Menor_De_Tres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+int a, b, c, menor;
+
+Console.WriteLine("Digite os valores para rodar o programa");
+Console.Write("Primeiro valor: ");
+a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Segundo valor: ");
+b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Terceiro valor: ");
+c = int.Parse(Console.ReadLine()!);
+
+if(a < b && a < c) {
+    menor = a;
+}else if(b < c) {
+    menor = b;
+}
+else {
+    menor = c;
+}
+
+Console.WriteLine("O menor numero é: " + menor);
+
+Console.WriteLine();
+Console.WriteLine("Fim do programa.");
