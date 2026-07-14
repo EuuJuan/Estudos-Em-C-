65c26a532643802bca4cbb9ca68cc2e7935f6dec
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:57:31 -0300

    Add files via upload

---
+++ Solução_Do_Problema_Soma_Impares/Solução_Do_Problema_Soma_Impares/Program.cs
@@ -0,0 +1,25 @@
+﻿int x, y, troca, soma;
+
+Console.WriteLine("Digite dois numeros para comecar o programa");
+
+Console.WriteLine("Digite aqui dois numeros: ");
+x = int.Parse(Console.ReadLine()!);
+y = int.Parse(Console.ReadLine()!);
+
+if(x > y) {
+    troca = x;
+    x = y;
+    y = troca;
+}
+
+soma = 0;
+
+for(int i = x+1; i < y; i++) {
+    if(i % 2 != 0) {
+        soma = soma + i;
+    }
+}
+
+Console.WriteLine("Soma dos impares = " + soma);
+
+Console.WriteLine("Programa finalizado");
