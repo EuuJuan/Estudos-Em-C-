8fc52d03fd8a3af3e7f053ffb882cfdb143db79b
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:46:40 -0300

    Add files via upload

---
+++ Solução_Do_Problema_Diagonal_Negativos/Solução_Do_Problema_Diagonal_Negativos/Program.cs
@@ -0,0 +1,31 @@
+﻿int N, cont;
+
+Console.WriteLine("Qual a ordem da Matriz");
+Console.Write("Digite aqui a ordem: ");
+N = int.Parse(Console.ReadLine()!);
+
+int[,] mat = new int [N, N];
+
+for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+        Console.Write("Elemento [" + i + "," + j + "]:");
+        mat[i, j] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+Console.WriteLine("Diagonal Principal:");
+for(int i = 0; i < N; i++) {
+    Console.Write(mat[i, i] + " ");
+}
+Console.WriteLine();
+
+cont = 0;
+for(int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++){
+        if (mat[i, j] < 0){
+            cont++;
+        }
+    }
+}
+
+Console.WriteLine("Quantidade de negativos = " + cont);
---
+++ Solução_Do_Problema_Idades/Solução_Do_Problema_Idades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+int idade1, idade2;
+String nome1, nome2;
+double media;
+
+Console.WriteLine("Dados da primeira pessoa:");
+Console.Write("Nome da Primeira Pessoa: ");
+nome1 = Console.ReadLine()!;
+Console.Write("Idade da Primeira Pessoa: ");
+idade1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Dados da Segunda pessoa:");
+Console.Write("Nome da Segunda Pessoa: ");
+nome2 = Console.ReadLine()!;
+Console.Write("Idade da Segunda Pessoa: ");
+idade2 = int.Parse(Console.ReadLine()!);
+
+media = (idade1 + idade2) / 2.0;
+
+Console.WriteLine("A idade media de " + nome1 + " e " + nome2 + " eh de " + media.ToString("F1", CI) + " anos.");
