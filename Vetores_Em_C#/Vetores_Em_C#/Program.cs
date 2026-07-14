52fe816a4fb6fd1316362058a1167995dcf87732
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:59:40 -0300

    Add files via upload

---
+++ Vetores_Em_C#/Vetores_Em_C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+int N;
+
+Console.Write("Quantos Numeros voce vai digitar: ");
+N = int.Parse(Console.ReadLine()!);
+
+double[] vet = new double [N];
+
+for(int i = 0; i < N; i++) {
+    Console.Write("Digite um Numero: ");
+    vet[i] = double.Parse(Console.ReadLine()!, CI);
+}
+
+Console.WriteLine();
+Console.WriteLine("Numeros Digitados São: ");
+for(int i = 0; i < N; i++) {
+    Console.WriteLine(vet[i].ToString("F1", CI));
+}
