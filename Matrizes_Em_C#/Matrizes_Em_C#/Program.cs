15dad23c22487506f4bda80d8fc5ba766787ceef
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:43:30 -0300

    Add files via upload

---
+++ Matrizes_Em_C#/Matrizes_Em_C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+int M, N;
+
+Console.Write("Quantas linhas vai ter a matriz: ");
+M = int.Parse(Console.ReadLine()!);
+Console.Write("Quantas colunas vai ter a matriz: ");
+N = int.Parse(Console.ReadLine()!);
+
+int[,] mat = new int[M, N];
+
+for(int i = 0; i < M; i++) {
+    for(int j = 0; j < N; j++) {
+        Console.Write("Elemento[" + i + "," + j + "]: ");
+        mat[i, j] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Matriz Digitadas: ");
+for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+        Console.Write(mat[i, j] + " ");
+    }
+    Console.WriteLine();
+}
