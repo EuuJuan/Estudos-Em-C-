fc29a7622ec4107555ab5cc99a7cb5c4b23bf88e
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:45:35 -0300

    Add files via upload

---
+++ Saida_De_Dados/Saida_De_Dados/Program.cs
@@ -0,0 +1,34 @@
+﻿//Biblioteca necessaria para uma porte do teste
+using System.Globalization;
+
+//Não ira ter a quebra de linha vai mostrar na tela tudo junto
+/*Console.Write("Bom dia");
+Console.Write("Boa noite");*/
+
+//Vai ter a quebra de linha evitando que fique tudo junto
+/*Console.WriteLine("Bom dia");
+Console.WriteLine("Boa noite");*/
+
+/*int x, y;
+x = 10;
+y = 20;
+Console.WriteLine(x);
+Console.WriteLine(y);*/
+
+/*CultureInfo CI = CultureInfo.InvariantCulture;
+double x;
+x = 2.3456;
+Console.Write(x.ToString("F2", CI));*/
+
+/*CultureInfo CI= CultureInfo.InvariantCulture;
+int idade;
+double salario;
+string nome;
+char sexo;
+
+idade = 32;
+salario = 4560.9;
+nome = "Maria Silva";
+sexo = 'F';
+
+Console.WriteLine("A funcionaria " + nome + ", sexo " + sexo + ", ganha " + salario.ToString("F2", CI) + " e tem " + idade + " anos");*/
---
+++ Solução_Do_Problema_Crescente/Solução_Do_Problema_Crescente/Program.cs
@@ -0,0 +1,21 @@
+﻿int x, y;
+
+Console.WriteLine("Digite dois numeros para o programa começar");
+
+Console.WriteLine("Digite dois numeros: ");
+x = int.Parse(Console.ReadLine()!);
+y = int.Parse(Console.ReadLine()!);
+
+while(x != y) {
+    if(x < y) {
+        Console.WriteLine("Crescente!");
+    }
+    else {
+        Console.WriteLine("Decrescente!");
+    }
+    Console.WriteLine("Digite outros dois numeros: ");
+    x = int.Parse(Console.ReadLine()!);
+    y = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine("Programa finalizado.");
