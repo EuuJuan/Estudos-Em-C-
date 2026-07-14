bea0e53be8aa7d911d85e4aa66dece9e22155dc3
Juan Reno <142558689+EuuJuan@users.noreply.github.com>
2026-07-14 15:44:26 -0300

    Add files via upload

---
+++ Processamento_E_Casting/Processamento_E_Casting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+/*int x, y;
+x = 5;
+y = 2 * x;
+
+Console.WriteLine(x);
+Console.WriteLine(y);*/
+
+/*int x;
+double y;
+x = 5;
+y = 2 * x;
+
+Console.WriteLine(x);
+Console.WriteLine(y.ToString("F1", CI));*/
+
+/*double b1, b2, h, area;
+b1 = 6.0;
+b2 = 8.0;
+h = 5.0;
+
+area = (b1 + b2) / 2.0 * h;
+Console.WriteLine(area.ToString("F2", CI)); */
+
+/*int a, b; 
+double resultado;
+
+a = 5;
+b = 2;
+resultado = (double) a / b;
+Console.WriteLine(resultado); */
+
+/*double a;
+int b;
+a = 5.0;
+b = (int)a;
+
+Console.WriteLine(b); */
